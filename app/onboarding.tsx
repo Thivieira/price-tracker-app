752654a7c41550103fd66d1735fcbb84f5095d11
Thiago Vieira <thiago.a.vieira@hotmail.com>
2025-02-03 21:48:23 -0300

    fix onboarding complete behavior

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,20 +1,21 @@
 import { useRouter } from 'expo-router';
 import SlideScreen from '@/components/onboarding/slide-screen';
 import { useOnboarding } from '../contexts/OnboardingContext';
+import { useEffect } from 'react';
 
 export default function OnboardingScreen() {
   const router = useRouter();
   const { isOnboardingComplete, isLoading } = useOnboarding();
 
+  useEffect(() => {
+    if (!isLoading && isOnboardingComplete) {
+      router.replace('/signup');
+    }
+  }, [isLoading, isOnboardingComplete]);
+
   if (isLoading) {
     return null; // Or a loading spinner
   }
 
-  if (isOnboardingComplete) {
-    router.replace('/');
-    return null;
-  }
-
   return <SlideScreen />
-
 }
